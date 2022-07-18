ca0d282afe87a6ef06180304b0f2fb8f84275301
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2022-07-18 11:15:04 -0300

    signIn screen implemented 🆗

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME} >
+      <StatusBar barStyle='light-content' translucent backgroundColor="transparent" />
+      {
+        fontsLoaded ? <SignIn /> : <Loading />
+      }
+    </NativeBaseProvider>
+  );
+}
